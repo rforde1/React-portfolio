313fcf7ec61f44645ed803c38301f21887285694
ramiforde <ramiforde@gmail.com>
2020-05-16 09:20:53 -0400

    Procfile

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -14,6 +14,7 @@ export default class Porfolio extends Component {
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
                     <a href="#modal-01">
+                      
                       <img src={`${item.imgurl}`} alt="gitimgurl" className="item-img"/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
