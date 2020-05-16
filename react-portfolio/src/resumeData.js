fe023276fcfbb570af3f82bb4e39e16e026c3c47
ramiforde <ramiforde@gmail.com>
2020-05-16 09:19:36 -0400

    Updated react-portfolio

---
+++ react-portfolio/src/resumeData.js
@@ -0,0 +1,67 @@
+let resumeData = {
+    "imagebaseurl":"http://github.com/rforde1",
+    "name": "Rami Forde",
+    "role": "Full Stack Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I enjoy dabbling in new Full Stack technologies, learning new ways to improve code, and simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rami-forde-6010a0197/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/rforde1",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"My name is Rami Forde. I enjoy being creative and outdoor activites. Im currently apart-time student at Georgia State pursuing my undergrad degree and a full time Veterinary Technician. I hope to one day build unique applications that will contribute to the health and wellness of animals around the world. I enjoy doing team activities with friend and build strong bonds. I hope to one day travel other countries to make new friends and create robust applications with people around the world.",
+    "address":"Atlanta",
+    "website":"http://github.com/rforde1",
+    "education":[
+      {
+        "UniversityName":"Georgia Tech Institute of Technology",
+        "specialization":"Full Stack Development Bootcamp",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2020",
+        "Achievements":"Certificate"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Collier Animal Hospital",
+        "specialization":"Veterinary Technician",
+        "MonthOfLeaving":"Present",
+        "YearOfLeaving":"Present",
+        "Achievements":"K-laser Certified, Proheart Injection Certified"
+      },
+      {
+        "CompanyName":"Woodland Emergency Animal Hospital",
+        "specialization":"Veterinary Technician",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2018",
+        "Achievements":"K-laser Certification"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Rick and Morty",
+        "description":"This application allows users to play trivia games, search characters, and search where they can watch the show in any country.",
+        "imgurl":"images/rick-and-morty.png"
+      },
+      {
+        "name":"Archer Forum",
+        "description":"Allows users to post and reply to any post on any season of archer. Bringing show lovers all in one space",
+        "imgurl":"images/archer-forum.png"
+      },
+      {
+        "name":"CHIPPER",
+        "description":"Application allows users to keep track of their pets microchip # also allowing rescue, shelters, and veterinarians find their information if their pet is lost",  
+        "imgurl":"images/chipper-screenshot.png"
+      },
+    ]
+  }
+  
+  export default resumeData
